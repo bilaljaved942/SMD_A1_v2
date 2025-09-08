931ca0717fec47a90963f22c8ed12b134a35233f
bilaljaved942 <bilal.javed942@gmail.com>
2025-09-08 19:58:54 +0500

    Commiting some changes and 2 other layouts

--- app/src/main/java/com/example/firstapp/MainActivity18.kt
+++ app/src/main/java/com/example/firstapp/MainActivity18.kt
@@ -9,24 +9,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity13 : AppCompatActivity() {
+class MainActivity18 : AppCompatActivity() {
     private companion object {
         const val SPLASH_TIMEOUT = 5000L
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main13)
+        setContentView(R.layout.activity_main18)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // Handler to delay the transition to MainActivity
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity14::class.java)
-            startActivity(intent)
-            finish()
-        }, SPLASH_TIMEOUT)
     }
 }
